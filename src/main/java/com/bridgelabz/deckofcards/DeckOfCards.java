f19b819f9d4af98440b0097a5019a8200bcdc65f
Adityachess <adityadone9398@gmail.com>
2021-10-28 23:44:31 +0530

    Added:There should be minimum two and maximum four players in the game.

--- src/main/java/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/main/java/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,33 +1,59 @@
 package com.bridgelabz.deckofcards;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DeckOfCards {
 
-	/**
-	 * Declaration of Array for suits and ranks
-	 */
-	static String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
-	static String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-	static int numOfCards = suits.length * ranks.length;
-	static String[] deck = new String[numOfCards];
+	public static final Scanner scanner = new Scanner(System.in);
+	public ArrayList<String> cardsDeck = new ArrayList<>();
 
-	public void initializationOfDeck() {
-		/**
-		 * initializing the deck
-		 */
-		for (int i = 0; i < ranks.length; i++) {
-			for (int j = 0; j < suits.length; j++) {
-				deck[suits.length * i + j] = ranks[i] + " of " + suits[j];
-			}
-		}
+	public void welcome() {
+		System.out.println("Welcome to the gaming world of Deck of Cards");
 	}
 
-	public static void main(String[] args) {
+	/**
+	 * In this method we have initialized the size of cards.
+	 */
 
-		/**
-		 * calling the method by creating object
-		 */
-		DeckOfCards object = new DeckOfCards();
-		object.initializationOfDeck();
-	
+	public void deckOfCards() {
+		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace" };
+		int numOfCards = suits.length * ranks.length;
+		System.out.println("\nNumber of cards in the deck:" + numOfCards);
+		for (int i = 0; i < ranks.length; i++) { // First we have to iterate for loop for ranks starting from index 0.
+			for (int j = 0; j < suits.length; j++) { // Now we have to itreate the suits for all the indexes of ranks
+				cardsDeck.add(ranks[i] + "----->" + suits[j]);
+			}
+		}
+		toDisplay(cardsDeck);
+	}
+
+	/**
+	 * Created a display method to print the array list of cardsDeck.
+	 */
+
+	public static void toDisplay(ArrayList<String> cardsDeck) {
+		System.out.println("\nCards in Deck:");
+		for (String element : cardsDeck) {
+			System.out.println(element);
+		}
+		System.out.println();
+	}
+
+	/**
+	 * In this we have defined no of players should be minimum 2 and maximum 4.
+	 */
+
+	public void noOfPlayers() {
+		System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
+		int player = scanner.nextInt();
+		if (player >= 2 && player <= 4) {
+			System.out.println("\n" + player + " players will play the game");
+		} else {
+			System.out.println("Please enter number of players in the Range");
+			this.noOfPlayers();
+			scanner.close();
+		}
 	}
 }
---
+++ src/main/java/com/bridgelabz/deckofcards/DeckOfCardsMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.deckofcards;
+
+public class DeckOfCardsMain {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Calling the method by creating the object using class name.
+		 */
+		DeckOfCards deckOfCardsGame = new DeckOfCards();
+		deckOfCardsGame.welcome();
+		deckOfCardsGame.deckOfCards();
+		deckOfCardsGame.noOfPlayers();
+	}
+
+}
